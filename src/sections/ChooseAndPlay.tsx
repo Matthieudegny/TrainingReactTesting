f40268ef46c87980671efc2dea4fd9a0a95574e0
Telmo Sampaio <telmoasampaio@gmail.com>
2022-11-26 14:04:51 +0000

    wrapping components with context

--- src/sections/ChooseAndPlay.tsx
+++ src/sections/ChooseAndPlay.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './ChooseAndPlay.module.css';
 import HandSelection from '../components/HandSelection';
+import { useOptions } from '../context/optionsContext';
 
 import { FaRegHandPaper, FaRegHandRock, FaRegHandScissors } from 'react-icons/fa';
 
 const ChooseAndPlay = () => {
+  const optionsContext = useOptions();
+
+  const HanpOptionsArray = optionsContext.options.map((hand) => {
+    return <HandSelection name={hand.name} icon={hand.icon} />;
+  });
+
   return (
     <>
-      <div className={styles.choiceBtnCtn}>
-        <HandSelection name="rock" icon={<FaRegHandRock size={60} />} />
-        <HandSelection name="paper" icon={<FaRegHandPaper size={60} />} />
-        <HandSelection name="scissors" icon={<FaRegHandScissors size={60} />} />
-      </div>
+      <div className={styles.choiceBtnCtn}>{HanpOptionsArray}</div>
       <button className={styles.playBtn}>Play</button>
     </>
   );
