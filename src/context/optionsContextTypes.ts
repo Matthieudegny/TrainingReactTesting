561780bc3dc775cc321f0c5beb63f72fab837d74
Telmo Sampaio <telmoasampaio@gmail.com>
2022-11-26 13:39:45 +0000

    adding context

---
+++ src/context/optionsContextTypes.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export enum HandOption {
+  rock = 'rock',
+  paper = 'paper',
+  scissors = 'scissors',
+}
+
+export interface IOptions {
+  name: HandOption;
+  icon: JSX.Element;
+}
+
+export interface IoptionsContext {
+  options: IOptions[];
+}
+
+export interface Props {
+  children: React.ReactNode;
+}
