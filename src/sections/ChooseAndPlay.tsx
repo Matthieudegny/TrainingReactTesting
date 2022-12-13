2e138c70377fe4acd3921d304ca1ff14e61a7a60
Telmo Sampaio <telmoasampaio@gmail.com>
2022-12-13 12:16:13 +0000

    generating computer rand number

--- src/sections/ChooseAndPlay.tsx
+++ src/sections/ChooseAndPlay.tsx
@@ -1,6 +1,7 @@
 import styles from './ChooseAndPlay.module.css';
 import HandSelection from '../components/HandSelection';
 import { useOptions } from '../context/optionsContext';
+import { generateComputerHand } from '../utils/randomNumber';
 
 const ChooseAndPlay = () => {
   const optionsContext = useOptions();
@@ -9,10 +10,17 @@ const ChooseAndPlay = () => {
     return <HandSelection name={hand.name} icon={hand.icon} handChoiceIndex={i} />;
   });
 
+  const play = () => {
+    const randNumber = generateComputerHand();
+    console.log('the computer rand number is: ', randNumber);
+  };
+
   return (
     <>
       <div className={styles.choiceBtnCtn}>{HanpOptionsArray}</div>
-      <button className={styles.playBtn}>Play</button>
+      <button className={styles.playBtn} onClick={play}>
+        Play
+      </button>
     </>
   );
 };
---
+++ src/utils/randomNumber.test.ts
@@ -0,0 +1,11 @@
+import { describe, vi } from 'vitest';
+import { generateComputerHand } from './randomNumber';
+
+describe('randomNumber', () => {
+  it('should create a random number', () => {
+    const randNumber = generateComputerHand();
+
+    expect(randNumber).toBeLessThanOrEqual(2);
+    expect(randNumber).toBeGreaterThanOrEqual(0);
+  });
+});
